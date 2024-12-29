28e8fc2d0ec9c790daa5deb78e53e5f920dabc07
Hanie Bp <haniebaratpoor@gmail.com>
2024-12-29 13:17:48 +0330

    change next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['dummyjson.com', 'cdn.dummyjson.com'], 
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dummyjson.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.dummyjson.com',
+      },
+    ],
   },
 };
 
